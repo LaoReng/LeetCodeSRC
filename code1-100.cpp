868e5a476895778515ccdbc390360795610d1b90
维东 鲁 <lwd15046543174@outlook.com>
2024-08-18 21:06:50 +0800

    update

--- code1-100.cpp
+++ code1-100.cpp
@@ -1751,5 +1751,35 @@ private:
 		return res[m - 1][n - 1];
 	}
 
+///*** No.63
+	int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        // 只需要将有石头的地方路径变为0
+        if(obstacleGrid[0][0]==1)return 0;
+		int x = 1;
+		for (int i = 0; i < obstacleGrid[0].size(); i++) {
+			if (obstacleGrid[0][i] == 1) {
+				x = 0;
+			}
+			obstacleGrid[0][i] = x;
+		}
+		x = 1;
+		for (int i = 1; i < obstacleGrid.size(); i++) {
+			if (obstacleGrid[i][0] == 1) {
+				x = 0;
+			}
+			obstacleGrid[i][0] = x;
+		}
 
+		for (int i = 1; i < obstacleGrid.size(); i++) {
+			for (int j = 1; j < obstacleGrid[0].size(); j++) {
+				if (obstacleGrid[i][j] == 1) {
+					obstacleGrid[i][j] = 0;
+				}
+				else {
+					obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1];
+				}
+			}
+		}
+		return obstacleGrid[obstacleGrid.size() - 1][obstacleGrid[0].size() - 1];
+	}
 };
