bcdc92f78a2dc267a23d8c0f4f4c0af93b55dbb2
维东 鲁 <lwd15046543174@outlook.com>
2024-09-06 20:56:10 +0800

    update

--- code101-200.cpp
+++ code101-200.cpp
@@ -1,5 +1,32 @@
 class Solution {
 public:
+///*** No.102
+	vector<vector<int>> levelOrder(TreeNode* root) {
+        // 广度优先搜索，学习博客https://blog.csdn.net/weixin_43955293/article/details/126445861
+		std::vector<std::vector<int>> res;
+		if (root == NULL) {
+			return res;
+		}
+		std::list<TreeNode*> NodeList = {root};
+		do {
+			std::vector<int> item;
+			int size = NodeList.size();
+			for (int i = 0; i < size; ++i) {
+				TreeNode* temp = NodeList.front();
+				NodeList.pop_front();
+                if(temp == NULL)
+                    continue;
+				item.push_back(temp->val);
+				if (temp->left != NULL)
+					NodeList.push_back(temp->left);
+				if (temp->right != NULL)
+					NodeList.push_back(temp->right);
+			}
+			res.push_back(item);
+		} while (NodeList.size() > 0);
+		return res;
+	}
+
 ///*** No.126
     int fib(int n) {
 			if (n <= 0)return 0;
