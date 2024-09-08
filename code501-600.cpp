111352b3e786128e1d5999d4f9cecd5489341ae7
维东 鲁 <lwd15046543174@outlook.com>
2024-09-08 18:21:22 +0800

    update

--- code501-600.cpp
+++ code501-600.cpp
@@ -1,16 +1,20 @@
-class Solution {
+class Solution
+{
 public:
 ///*** No.509
-    int fib(int n) {
-        if (n <= 0)return 0;
-	    if (n == 1 || n == 2)
-		    return 1;
-	    int n1 = 1, n2 = 1, temp;
-	    for (int i = 3; i <= n; i++) {
-		    temp = n1 + n2; // **如果题目要求取余，就在这取余，不要在最后取余会溢出**
-		    n1 = n2;
-		    n2 = temp;
-	    }
-	    return temp;
-    }
+	int fib(int n)
+	{
+		if (n <= 0)
+			return 0;
+		if (n == 1 || n == 2)
+			return 1;
+		int n1 = 1, n2 = 1, temp;
+		for (int i = 3; i <= n; i++)
+		{
+			temp = n1 + n2; // **如果题目要求取余，就在这取余，不要在最后取余会溢出**
+			n1 = n2;
+			n2 = temp;
+		}
+		return temp;
+	}
 };
